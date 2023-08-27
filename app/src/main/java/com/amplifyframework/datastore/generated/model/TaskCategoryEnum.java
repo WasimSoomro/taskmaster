a5bb6d9c16a5a6a3f78a60fef3f0cb1343e2980b
Wasim Soomro <103798599+WasimSoomro@users.noreply.github.com>
2023-08-27 17:42:00 -0600

    Lab32

---
+++ app/src/main/java/com/amplifyframework/datastore/generated/model/TaskCategoryEnum.java
@@ -0,0 +1,9 @@
+package com.amplifyframework.datastore.generated.model;
+/** Auto generated enum from GraphQL schema. */
+@SuppressWarnings("all")
+public enum TaskCategoryEnum {
+  NEW,
+  ASSIGNED,
+  IN_PROGRESS,
+  COMPLETE
+}
